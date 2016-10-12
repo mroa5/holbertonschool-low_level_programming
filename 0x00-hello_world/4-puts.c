fcf998b22bcf741efef2724421d85be937a98dfa
mroa5 <michael.roa@holbertonschool.com>
2016-10-12 23:06:06 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-int main(void)
+/*
+ * print with puts
+ * return 0
+ */
+main(void)
 {
-	/* Test */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
