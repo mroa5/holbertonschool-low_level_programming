698d0249bc38a0f198d6b21b0383dc4f4554f2b2
mroa5 <michael.roa@holbertonschool.com>
2016-10-13 01:37:36 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  * printf
- * return 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
