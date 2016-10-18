dae9cf62c0bbfce4337593783ef16386a7c20e2e
mroa5 <michael.roa@holbertonschool.com>
2016-10-18 21:18:01 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _islower checks if c char is lowercase char
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 173)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
