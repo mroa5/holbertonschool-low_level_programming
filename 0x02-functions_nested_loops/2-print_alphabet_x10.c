b70fa934d293a9483acf42be46d4371546d635a6
mroa5 <michael.roa@holbertonschool.com>
2016-10-18 19:44:09 +0000

    alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alpha 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int x,y;
+	x = 0;
+	while (x < 10)
+	{
+		y = 'a';
+		while (y <= 'z')
+		{
+			_putchar(y);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
