cbb9c7ce3f5dd0e2e480f2b2a95c47b14c55c7d2
mroa5 <michael.roa@holbertonschool.com>
2016-10-19 02:52:08 +0000

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * times_table - prints 0 - 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int x, y;
+
+	x = 0;
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			if ((x * y) < 10)
+			{
+				if (y != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar((x * y) + '0');
+			}
+			else
+			{
+				_putchar((x * y) / 10 + '0');
+				_putchar((x * y) % 10 + '0');
+			}
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
