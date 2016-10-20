4564f6abcf5c259bf9988d8fddd36b2e02430bdc
mroa5 <michael.roa@holbertonschool.com>
2016-10-20 23:34:24 +0000

    print line

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_line - print _
+ *
+ */
+void print_line(int n)
+{
+        int x;
+
+	x = 0;
+	while (x < n)
+	{
+		if (n >= 0)
+			_putchar('_');
+		else
+			_putchar('\n');
+		x++;
+	}
+	_putchar('\n');
+}
