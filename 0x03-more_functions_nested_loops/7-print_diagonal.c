bf18a6e91e03f5384dc128ce6cc4430f34b483cc
mroa5 <michael.roa@holbertonschool.com>
2016-10-21 00:04:39 +0000

    print diagonal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print \
+ * @n: number of \
+ *
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	x = 0;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	while (x < n)
+	{
+		y = x;
+		while (y > 0)
+		{
+			_putchar(' ');
+			y--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		x++;
+	}
+}
