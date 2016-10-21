60a22e987b4f75cafc329472bcf9463d70a6e033
mroa5 <michael.roa@holbertonschool.com>
2016-10-21 03:54:05 +0000

    print triangle

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,24 @@
  */
 void print_triangle(int size)
 {
-	int x, y, z;
+	int x, y;
 
 	x = 0;
 	if (size <= 0)
 		_putchar('\n');
 	while (x < size)
 	{
-		y = size - 1 - x;
-		z = x + 1;
-		while (y > 0)
+		y = size + 1;
+		while (y > x)
 		{
 			_putchar(' ');
 			y--;
 		}
-		while (z > 0)
+		y = 0;
+		while (y < x + 1)
 		{
 			_putchar('#');
-			z--;
+			y++;
 		}
 		_putchar('\n');
 		x++;
