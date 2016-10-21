b5c80b9e21eecf9ab136d715df1c9baa8d55e139
mroa5 <michael.roa@holbertonschool.com>
2016-10-21 00:50:30 +0000

    fizz buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	x = 1;
+	while (x < 100)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", x);
+		printf(" ");
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
