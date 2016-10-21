f7d2337a3657514b8338bab336b1e3adc54d7e51
mroa5 <michael.roa@holbertonschool.com>
2016-10-21 02:36:08 +0000

    print triangle

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_triangle - print triangle
+ * @size: size of triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	x = 0;
+	while (x < size)
+	{
+		y = size - 1 - x;
+		z = x + 1;
+		while (y > 0)
+		{
+			_putchar(' ');
+			y--;
+		}
+		while (z > 0)
+		{
+			_putchar('#');
+			z--;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
