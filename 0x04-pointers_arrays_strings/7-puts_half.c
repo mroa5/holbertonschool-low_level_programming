ed3c0cc149221b9b8bea07f37f0d7f9dd689fcfe
mroa5 <michael.roa@holbertonschool.com>
2016-10-25 22:06:46 +0000

    half

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * puts_half - print second half of string
+ * @str: string
+ *
+ */
+void puts_half(char *str)
+{
+	int len, half;
+
+	len = 0;
+	half = 0;
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 != 0)
+		half = (len - 1) / 2;
+	else
+		half = len / 2;
+	while (half < len)
+	{
+		_putchar(str[half]);
+		half++;
+
+	}
+	_putchar('\n');
+}
