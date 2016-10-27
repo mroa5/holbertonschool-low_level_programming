b865d1714de4a08b391710d7f5581e0151f86f52
mroa5 <michael.roa@holbertonschool.com>
2016-10-27 20:58:31 +0000

    cat str

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcat - concat two strings
+ * @dest: destination
+ * @src: source
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != '\0')
+	{
+		dest[x++] = src[y++];
+	}
+	return(dest);
+}
