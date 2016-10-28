c73aeba24c33474f0b6acb02497091e262475853
mroa5 <michael.roa@holbertonschool.com>
2016-10-28 01:15:55 +0000

    to upper

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * string_toupper - lower letters to upper
+ * @p: str
+ * Return: p
+ */
+
+char *string_toupper(char *p)
+{
+	int x;
+
+	x = 0;
+	while (p[x] != '\0')
+	{
+		if (p[x] >= 'a' && p[x] <= 'z')
+			 p[x] -= 32;
+		x++;
+	}
+	return (p);
+}
