47da76da0bcab532ccb18ba2b5439157a14df17f
mroa5 <michael.roa@holbertonschool.com>
2016-10-28 02:34:01 +0000

    cap string

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,19 @@
 #include "holberton.h"
+
+/**
+ * seperator - checks if its a seperator
+ * @c: char to check
+ * Return: 1 if seperator
+ */
+int seperator(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' ||
+	        c == '.' || c == '!' || c == '?' || c == '"' || c == '(' ||
+	    c == ')' || c == '{' || c == '}')
+		return (1);
+	return (0);
+}
+
 /**
  * cap_string - cap words of string
  * @p: string
@@ -13,7 +28,7 @@ char *cap_string(char *p)
 		p[x] -= 32;
 	while (p[x] != '\0')
 	{
-		if (p[x] <= '/' && (p[x + 1] >= 'a' && p[x + 1] <= 'z'))
+		if (seperator(p[x]) == 1 && (p[x + 1] >= 'a' && p[x + 1] <= 'z'))
 		{
 			p[x + 1] -= 32;
 			x++;
