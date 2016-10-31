8a8fd28b4b672e3dd3d7ce914b00310706673c64
mroa5 <michael.roa@holbertonschool.com>
2016-10-31 21:37:29 +0000

    strchr

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,10 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
-			return ((char *) s);
-		}
+			return (s);
 		s++;
 	}
 	if (*s == c)
-		return ((char *) s);
+		return (s);
 	return ('\0');
 }
