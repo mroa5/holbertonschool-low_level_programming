62280a3c13cf4344ffdc8b7afeece0bd399f15dc
mroa5 <michael.roa@holbertonschool.com>
2016-10-31 21:23:09 +0000

    strchr

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - print first instance of char c
+ * @s: char str
+ * @c: char to find
+ * Return: char found or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return ((char *) s);
+		}
+		s++;
+	}
+	return (0);
+}
