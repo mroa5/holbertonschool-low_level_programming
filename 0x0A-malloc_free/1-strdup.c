6b2ef26385c2870754e0b505f7ce05d027860930
mroa5 <michael.roa@holbertonschool.com>
2016-11-09 07:42:28 +0000

    strdup

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - length of str
+ * @s: string
+ * Return: length of str
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strcpy - copy str
+ * @dest: temp string
+ * @src: source string
+ *
+ * Return: dest
+ */
+char _strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (*dest);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a
+ * parameter.
+ *
+ * @str: variable string
+ * Return: string otherwise NULL
+ */
+char *_strdup(char *str)
+{
+	char *dupe;
+
+	dupe = malloc(_strlen(str) + 1);
+	if (dupe == NULL)
+	{
+		return (NULL);
+	}
+	_strcpy(dupe, str);
+	return (dupe);
+}
