fd06f2344c23bad12d5ba7ceb5ad461b1a1b9fcf
mroa5 <michael.roa@holbertonschool.com>
2016-11-09 07:40:50 +0000

    create array

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _memset - set first n chars to s
+ * @s: char string
+ * @b: char
+ * @n: number of times char should be used
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
+
+/**
+ * create_array - creates a block of memory using
+ *  malloc and fills it with a character
+ *
+ * @size: array size
+ * @c: character to put in array
+ * Return: array otherise NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int arsize;
+
+	arsize = (sizeof(char) * (int)size);
+	s = malloc(arsize);
+	_memset(s, c, size);
+	return (s);
+}
