bf6a349d1ef84a049cd0a00c844d262735798f89
mroa5 <michael.roa@holbertonschool.com>
2016-11-10 22:52:16 +0000

    str nconcat

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -53,9 +53,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, y;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = 0;
 	if (s2 == NULL)
-		s2 = "";
+		s2 = 0;
 	s = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (s == NULL)
 		return (NULL);
