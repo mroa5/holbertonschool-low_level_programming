db41104482f595d8a78025be9fa80ceb6798aafd
mroa5 <michael.roa@holbertonschool.com>
2016-11-10 22:23:51 +0000

    string concat

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,77 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - length of str
+ * @s: string
+ * Return: length of str
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strncat - get n bytes from str
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+	while (dest[x] != '\0')
+		x++;
+	while (y != n && src[y] != '\0')
+	{
+		dest[x++] = src[y++];
+	}
+	dest[x] = '\0';
+	return (dest);
+}
+/**
+ * string_nconcat - concat two strings
+ * @s1: str 1
+ * @s2: str 2
+ * @n: n bytes of str 2 to concat
+ *
+ * Return: concat str
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i, x, y;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	s = malloc(_strlen(s1) + _strlen(s2) + 1);
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	_strncat(s, s1, i);
+	y = _strlen(s2) + 1;
+	if (n >= y)
+	{
+		x = 0;
+		while (s2[x] != '\0')
+			x++;
+		_strncat(s, s2, x);
+	}
+	else
+		_strncat(s, s2, n);
+	return (s);
+}
