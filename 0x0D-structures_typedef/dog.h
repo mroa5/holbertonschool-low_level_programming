317742f1d6de6783643ff6cddeb7878ed65cf10c
mroa5 <michael.roa@holbertonschool.com>
2016-11-15 06:42:37 +0000

    dog h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of dog
+ * @name: char str
+ * @age: float age
+ * @owner: char str
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+#endif
