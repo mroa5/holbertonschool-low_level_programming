99a65882be5eddc0b9725b5d6dbb66d13afc7007
mroa5 <michael.roa@holbertonschool.com>
2016-11-15 07:13:15 +0000

    new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * new_dog: new dog struct
+ * new_dog - new dog struct
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
- * Return: Doggy
+ * Return: doggy
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
