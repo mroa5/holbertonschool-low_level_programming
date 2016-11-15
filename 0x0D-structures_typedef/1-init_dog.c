57303daad8c63d554c47eaa29f87460517de651b
mroa5 <michael.roa@holbertonschool.com>
2016-11-15 06:45:57 +0000

    init dog

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog: initialize dog structure
+ * @d: dog structure passed
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
