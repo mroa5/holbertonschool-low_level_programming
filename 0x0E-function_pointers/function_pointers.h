ab5fc260f9b6913243b139159bbd7cbdcf7b83e5
mroa5 <michael.roa@holbertonschool.com>
2016-11-16 21:39:33 +0000

    guarded

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
-#ifndef FCTPOINT_H_
-#define FCTPOINT_H_
+#ifndef FPOINTERS
+#define FPOINTERS
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif
+#endif /* FPOINTERS */
