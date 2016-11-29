586fa3f1228d8466df12b56a9e717c8919798e29
mroa5 <michael.roa@holbertonschool.com>
2016-11-29 07:18:53 +0000

    node end

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * _strlen - length of a str
+ * @s: str
+ * Return: length of str
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * add_node_end - add new node to end of a list_t
+ * @head: start of list
+ * @str: string to add to node
+ * Return: new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode;
+	list_t *temp;
+
+	newnode = malloc(sizeof(list_t));
+	if (*head == NULL)
+		return (NULL);
+	newnode->str = strdup(str);
+	newnode->len = _strlen(str);
+	if (*head == NULL)
+	{
+		newnode->next = *head;
+		*head = newnode;
+	}
+	else
+	{
+		newnode->next = NULL;
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newnode;
+	}
+	return (newnode);
+}
