3207cff95e11f406b852f927b08ab427d520a6f1
mroa5 <michael.roa@holbertonschool.com>
2016-11-29 06:51:41 +0000

    help

---
+++ 0x11-singly_linked_lists/help_func.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strlen - length of a string
+ * @s: string passed
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
