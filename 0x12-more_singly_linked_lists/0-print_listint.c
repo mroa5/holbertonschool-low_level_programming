b581ffb0203fda82f2823a06795c91612bc60bc5
mroa5 <michael.roa@holbertonschool.com>
2016-12-02 07:30:46 +0000

    update

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,25 @@
 #include "lists.h"
 /**
+ * print_listint - prints all elements in listint_t
+ * @h: pointer
  *
- *
- *
+ * Return: number of elements
  */
 size_t print_listint(const listint_t *h)
 {
 	int i;
 
-	for (i = 0; h != NULL; i++)
+	i = 0;
+	if (h)
 	{
+		while (h->next)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
 		printf("%d\n", h->n);
-		h = h->next;
+		i++;
 	}
 	return (i);
-}
+}}
