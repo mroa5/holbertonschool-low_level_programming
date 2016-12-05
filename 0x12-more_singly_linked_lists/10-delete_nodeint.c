e4b8773ef7f6b7682737e52ac27b75af7054cebc
mroa5 <michael.roa@holbertonschool.com>
2016-12-05 00:29:55 +0000

    delete node

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *keep;
 	unsigned int i;
 
-	if(*head == NULL)
+	if (*head == NULL)
 		return (-1);
 	temp = *head;
 	i = 0;
